7a565a1485dee4423e37bc2878300e49faac9358
cmlee <bal.dongdongdong@gmail.com>
2022-06-03 23:54:56 +0900

    :recycle: refactor code

--- index.js
+++ index.js
@@ -50,9 +50,7 @@ const delay = (ms) => new Promise((r) => setTimeout(r, ms));
       .filter(
         (item) =>
           KEYWORDS.some((k) => item.title.includes(k)) &&
-          item.createdAt
-            .tz(TIMEZONE)
-            .isAfter(dayjs().tz(TIMEZONE).subtract(30, 'minutes')),
+          item.createdAt.isAfter(dayjs().tz(TIMEZONE).subtract(30, 'minutes')),
       );
 
     await delay(1000);
