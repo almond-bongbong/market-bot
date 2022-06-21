db1ddcb6f3e00c19f25a7510c597e057e927a83b
cmlee <bal.dongdongdong@gmail.com>
2022-06-21 22:07:21 +0900

    :memo: add search keyword

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import timezone from 'dayjs/plugin/timezone.js';
 import { sendSlackMessage } from './utils.js';
 
 const TIMEZONE = 'Asia/Seoul';
-const KEYWORDS = ['면도기', '쉬크', '스타일러', '에어드레서', '딜라이트'];
+const KEYWORDS = ['면도기', '쉬크', '스타일러', '에어드레서', '딜라이트', '질레트'];
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
