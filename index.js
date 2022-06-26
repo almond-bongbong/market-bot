b95f2fbb7fe75b50bab1898bfd3af726f2cfd5a0
cmlee <bal.dongdongdong@gmail.com>
2022-06-27 08:30:04 +0900

    :zap: update check time interval

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const delay = (ms) => new Promise((r) => setTimeout(r, ms));
       .filter(
         (item) =>
           KEYWORDS.some((k) => item.title.includes(k)) &&
-          item.createdAt.isAfter(dayjs().tz(TIMEZONE).subtract(33, 'minutes')),
+          item.createdAt.isAfter(dayjs().tz(TIMEZONE).subtract(32, 'minutes')),
       );
 
     await delay(1000);
