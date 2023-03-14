fa6bbc33770342c83a9be8ab77d96648fe9c3405
cmlee <bal.dongdongdong@gmail.com>
2023-03-14 20:33:35 +0900

    :bug: add soon case

--- scraper/whisky-in.js
+++ scraper/whisky-in.js
@@ -11,7 +11,10 @@ export const scrapeHibikiIn = async () => {
     const contentDiv = $('#contents > .sub');
     const className = contentDiv.attr('class');
 
-    if (className.indexOf('sold_out') === -1) {
+    if (
+      className.indexOf('sold_out') === -1 &&
+      className.indexOf('soon') === -1
+    ) {
       await sendSlackMessage(
         '히비키 입고되었습니다. \n https://www.pocketcu.co.kr/product/detail/2022080021854?cateTyp=A&chldMealEvtYn=null',
         process.env.SLACK_HOTDEAL_CHANNEL_ID,
